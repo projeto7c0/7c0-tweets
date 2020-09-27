e8f0f04c41c1ec21f9f8b287cb00d1f302cbeda0
Lucas Lago <lago@nic.br>
2020-09-27 16:09:33 -0300

    atualização para não pegar candidatos com 0 tweets

--- tuitar_election.py
+++ tuitar_election.py
@@ -10,18 +10,19 @@ if __name__ == '__main__':
     print("Coletados " + str(len(candidatos)) + " arrobas para tuitar...")
 
     print("iniciando processo de tuites... ")
-    # twitter_election.tweet_start()
+    twitter_election.tweet_start()
 
     qtde_tweets = 0
     for candidato in candidatos:
         tweet_ids = database.get_tweets_ids_election(candidato)
-        last_tweet = twitter_election.tweet_start_arroba(candidato, len(tweet_ids))
-        for tweet_id in tweet_ids:
-            qtde_tweets += 1
-            id, tweet, handle, archive_url, creation_date = database.retrieve_tweet(tweet_id)
-            last_tweet = twitter_election.tweet(handle, tweet, archive_url, creation_date, id, last_tweet)
-            if not qtde_tweets%20:
-                time.sleep(3600)
-        
-        database.update_tweeted_election(tweet_ids)
-        twitter_election.tweet_end_arroba(candidato, last_tweet)
+        if len(tweet_ids > 0):
+            last_tweet = twitter_election.tweet_start_arroba(candidato, len(tweet_ids))
+            for tweet_id in tweet_ids:
+                qtde_tweets += 1
+                id, tweet, handle, archive_url, creation_date = database.retrieve_tweet(tweet_id)
+                last_tweet = twitter_election.tweet(handle, tweet, archive_url, creation_date, id, last_tweet)
+                if not qtde_tweets%20:
+                    time.sleep(3600)
+            
+            database.update_tweeted_election(tweet_ids)
+            twitter_election.tweet_end_arroba(candidato, last_tweet)
